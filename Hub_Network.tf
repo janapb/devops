f604d725d70ce18c468070d820a0dc793eb5eaaf
Janakiraman <admin.perumal1@softwareone.com>
2020-12-17 16:23:21 +0800

    Adding Resource

--- Hub_Network.tf
+++ Hub_Network.tf
@@ -23,3 +23,20 @@ resource "azurerm_subnet" "hub_subnet" {
   virtual_network_name = azurerm_virtual_network.poc_mgmt_vnet.name
   address_prefix       = lookup(element(var.subnet_prefix, count.index), "ip")
 }
+
+
+# Creates Network Security Group For Hub subnet 
+resource "azurerm_network_security_group" "nsg-hub" {
+  count               = length(var.subnet_prefix)
+  name                = "${lookup(element(var.subnet_prefix, count.index), "name")}-NSG"
+  location            = azurerm_resource_group.poc_rg.location
+  resource_group_name = azurerm_resource_group.poc_rg.name
+  tags                = var.comman_tags
+}
+
+# Associates jumpbox Network Security Group to subnet within Hub Network
+resource "azurerm_subnet_network_security_group_association" "hub-asso-nsg" {
+  count                     = length(var.subnet_prefix)
+  subnet_id                 = azurerm_subnet.hub_subnet[count.index].id
+  network_security_group_id = azurerm_network_security_group.nsg-hub[count.index].id
+}
