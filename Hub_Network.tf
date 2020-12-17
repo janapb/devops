0a27dbb84124104edf2f68e4a3038c4337d641cb
Janakiraman <admin.perumal1@softwareone.com>
2020-12-17 14:45:21 +0800

    Adding Resources

--- Hub_Network.tf
+++ Hub_Network.tf
@@ -6,3 +6,12 @@ resource "azurerm_resource_group" "poc_rg" {
   tags     = var.comman_tags
 }
 
+# Creates the Hub VNET
+resource "azurerm_virtual_network" "poc_mgmt_vnet" {
+  name                = var.mgmt_vnet_name
+  location            = azurerm_resource_group.poc_rg.location
+  resource_group_name = azurerm_resource_group.poc_rg.name
+  address_space       = [var.mgmt_vnet_cidr]
+  tags                = var.comman_tags
+}
+
