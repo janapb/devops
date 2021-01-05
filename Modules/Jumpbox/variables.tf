8e41336e11b7c221c70325922f0fada8f995127b
janapb <52954974+janapb@users.noreply.github.com>
2021-01-05 22:47:35 +0800

    Delete variables.tf

--- Modules/Jumpbox/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "app_os_jump" {
-  description = "Enter the OS related information for Application and Database"
-}
-
-variable "jmp-vm-win" {
-  description = "Enter the jumpbox server name"
-  default     = ["Jumpbox"]
-}
